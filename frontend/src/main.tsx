e4811d6e8abb7c9a1458dd21bb7f974cbb9d2a9e
Joar Grimstvedt <joarg@equinor.com>
2025-03-27 15:31:52 +0100

    DEV: Add additional linting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render( // eslint-disable-line @typescript-eslint/no-non-null-assertion
   <StrictMode>
     <App />
   </StrictMode>,
