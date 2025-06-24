1b6ed2c034c72f69e91b6ae07174a46dba39a274
Joar Grimstvedt <joarg@equinor.com>
2025-06-24 10:21:02 +0200

    ENH: Add styling as well as some more editing functionality

--- frontend/src/components/form.tsx
+++ frontend/src/components/form.tsx
@@ -1,12 +1,47 @@
-import { AnyFieldApi } from "@tanstack/react-form";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import {
+  Button,
+  DotProgress,
+  TextField as EdsTextField,
+  Icon,
+  Tooltip,
+} from "@equinor/eds-core-react";
+import { error_filled } from "@equinor/eds-icons";
+import { createFormHook } from "@tanstack/react-form";
+import {
+  QueryClient,
+  useMutation,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "react-toastify";
 import z from "zod/v4";
 
-import { useFieldContext } from "../utils/form";
+import { UserApiKeys } from "../client";
+import {
+  v1GetUserOptions,
+  v1GetUserQueryKey,
+  v1PatchApiKeyMutation,
+} from "../client/@tanstack/react-query.gen";
+import { fieldContext, formContext, useFieldContext } from "../utils/form";
+import { EditableTextFieldFormContainer } from "./form.style";
+
+Icon.add({ error_filled });
 
 export function TextField({
+  label,
+  placeholder,
+  isReadOnly,
   setSubmitDisabled,
 }: {
+  label: string;
+  placeholder?: string;
+  isReadOnly?: boolean;
   setSubmitDisabled: Dispatch<SetStateAction<boolean>>;
 }) {
   const field = useFieldContext<string>();
@@ -22,40 +57,171 @@ export function TextField({
   ]);
 
   return (
-    <>
-      <input
-        id={field.name}
-        type="text"
-        name={field.name}
-        value={field.state.value}
-        onBlur={field.handleBlur}
-        onChange={(e) => {
-          field.handleChange(e.target.value);
+    <EdsTextField
+      id={field.name}
+      name={field.name}
+      label={label}
+      readOnly={isReadOnly}
+      value={field.state.value}
+      placeholder={placeholder}
+      onBlur={field.handleBlur}
+      onChange={(e: ChangeEvent<HTMLInputElement>) => {
+        field.handleChange(e.target.value);
+      }}
+      {...(!field.state.meta.isValid && {
+        variant: "error",
+        helperIcon: <Icon name="error_filled" title="Error" />,
+        helperText: field.state.meta.errors
+          .map((err: z.ZodError) => err.message)
+          .join(", "),
+      })}
+    />
+  );
+}
+
+export function SubmitButton({
+  disabled,
+  isPending,
+}: {
+  disabled?: boolean;
+  isPending?: boolean;
+}) {
+  return (
+    <Tooltip
+      title={
+        disabled
+          ? "Value can be submitted when it has been changed and is valid"
+          : ""
+      }
+    >
+      <Button
+        type="submit"
+        aria-disabled={disabled}
+        onClick={(e) => {
+          if (disabled) {
+            e.preventDefault();
+          }
         }}
-      />
-      <FieldError field={field} />
-    </>
+      >
+        {isPending ? <DotProgress /> : "Submit"}
+      </Button>
+    </Tooltip>
   );
 }
 
-function FieldError({ field }: { field: AnyFieldApi }) {
-  return (
-    <>
-      {!field.state.meta.isValid && (
-        <span>
-          {field.state.meta.errors
-            .map((err: z.ZodError) => err.message)
-            .join(", ")}
-        </span>
-      )}
-    </>
-  );
-}
+const { useAppForm: useAppFormEditableTextField } = createFormHook({
+  fieldContext,
+  formContext,
+  fieldComponents: { TextField },
+  formComponents: { SubmitButton },
+});
 
-export function SubmitButton({ disabled }: { disabled?: boolean }) {
+export function EditableTextFieldForm({
+  apiKey,
+  label,
+  queryClient,
+  placeholder,
+  minLength,
+}: {
+  apiKey: keyof UserApiKeys;
+  label: string;
+  queryClient: QueryClient;
+  placeholder?: string;
+  minLength?: number;
+}) {
+  const [isReadonly, setIsReadonly] = useState(true);
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+  const { data } = useSuspenseQuery(v1GetUserOptions());
+  const { mutate, isPending } = useMutation({
+    ...v1PatchApiKeyMutation(),
+    meta: { errorMessage: "Error updating API key" },
+  });
+
+  const form = useAppFormEditableTextField({
+    defaultValues: {
+      [apiKey]: data.user_api_keys[apiKey] ?? "",
+    },
+    onSubmit: ({ formApi, value }) => {
+      mutate(
+        {
+          body: { id: apiKey, key: value[apiKey] },
+        },
+        {
+          onSuccess: (data) => {
+            toast.info(data.message);
+            void queryClient.refetchQueries({
+              queryKey: v1GetUserQueryKey(),
+            });
+            formApi.reset();
+            setIsReadonly(true);
+          },
+        },
+      );
+    },
+  });
+
   return (
-    <button type="submit" disabled={disabled}>
-      Submit
-    </button>
+    <EditableTextFieldFormContainer>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          void form.handleSubmit();
+        }}
+      >
+        <form.AppField
+          name={apiKey}
+          {...(minLength && {
+            validators: {
+              onBlur: z
+                .string()
+                .refine((val) => val === "" || val.length >= minLength, {
+                  error: `Value must be at least ${String(minLength)} characters long`,
+                }),
+            },
+          })}
+        >
+          {(field) => (
+            <field.TextField
+              label={label}
+              placeholder={placeholder}
+              isReadOnly={isReadonly}
+              setSubmitDisabled={setSubmitDisabled}
+            />
+          )}
+        </form.AppField>
+
+        <form.AppForm>
+          {isReadonly ? (
+            <Button
+              onClick={() => {
+                setIsReadonly(false);
+              }}
+            >
+              Edit
+            </Button>
+          ) : (
+            <>
+              <form.SubmitButton
+                disabled={submitDisabled}
+                isPending={isPending}
+              />
+              <Button
+                type="reset"
+                color="secondary"
+                variant="outlined"
+                onClick={(e) => {
+                  e.preventDefault();
+                  form.reset();
+                  setIsReadonly(true);
+                }}
+              >
+                Cancel
+              </Button>
+            </>
+          )}
+        </form.AppForm>
+      </form>
+    </EditableTextFieldFormContainer>
   );
 }
--- frontend/src/routes/user/keys.tsx
+++ frontend/src/routes/user/keys.tsx
@@ -1,102 +1,18 @@
-import { createFormHook } from "@tanstack/react-form";
-import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { Suspense, useState } from "react";
-import { toast } from "react-toastify";
-import z from "zod/v4";
+import { Suspense } from "react";
 
-import { UserApiKeys } from "../../client";
-import {
-  v1GetUserOptions,
-  v1GetUserQueryKey,
-  v1PatchApiKeyMutation,
-} from "../../client/@tanstack/react-query.gen";
 import { Loading } from "../../components/common";
-import { SubmitButton, TextField } from "../../components/form";
+import { EditableTextFieldForm } from "../../components/form";
 import { PageHeader, PageText } from "../../styles/common";
-import { fieldContext, formContext } from "../../utils/form";
+import { KeysForm } from "./keys.style";
 
 export const Route = createFileRoute("/user/keys")({
   component: RouteComponent,
 });
 
-const { useAppForm } = createFormHook({
-  fieldContext,
-  formContext,
-  fieldComponents: { TextField },
-  formComponents: { SubmitButton },
-});
-
-function EditableTextField({
-  apiKey,
-  minLength,
-}: {
-  apiKey: keyof UserApiKeys;
-  minLength?: number;
-}) {
-  const [submitDisabled, setSubmitDisabled] = useState<boolean>(true);
-  const { queryClient } = Route.useRouteContext();
-  const { data } = useSuspenseQuery(v1GetUserOptions());
-  const { mutate } = useMutation({
-    ...v1PatchApiKeyMutation(),
-    meta: { errorMessage: "Error updating API key" },
-  });
-
-  const form = useAppForm({
-    defaultValues: {
-      [apiKey]: data.user_api_keys[apiKey] ?? "",
-    },
-    onSubmit: ({ formApi, value }) => {
-      mutate(
-        {
-          body: { id: apiKey, key: value[apiKey] },
-        },
-        {
-          onSuccess: () => {
-            void queryClient.invalidateQueries({
-              queryKey: v1GetUserQueryKey(),
-            });
-            toast.info("API key updated");
-          },
-        },
-      );
-      formApi.reset();
-      setSubmitDisabled(true);
-    },
-  });
-
-  return (
-    <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          void form.handleSubmit();
-        }}
-      >
-        <form.AppField
-          name={apiKey}
-          {...(minLength && {
-            validators: {
-              onBlur: z
-                .string()
-                .refine((val) => val === "" || val.length >= minLength, {
-                  error: `Value must be at least ${String(minLength)} characters long`,
-                }),
-            },
-          })}
-        >
-          {(field) => <field.TextField setSubmitDisabled={setSubmitDisabled} />}
-        </form.AppField>
-        <form.AppForm>
-          <form.SubmitButton disabled={submitDisabled} />
-        </form.AppForm>
-      </form>
-    </>
-  );
-}
-
 function Content() {
+  const { queryClient } = Route.useRouteContext();
+
   return (
     <>
       <PageText $variant="ingress">
@@ -107,7 +23,15 @@ function Content() {
 
       <PageHeader $variant="h3">SMDA</PageHeader>
 
-      <EditableTextField apiKey="smda_subscription" minLength={16} />
+      <KeysForm>
+        <EditableTextFieldForm
+          apiKey="smda_subscription"
+          label="SMDA subscription key"
+          queryClient={queryClient}
+          placeholder="(not set)"
+          minLength={5}
+        />
+      </KeysForm>
     </>
   );
 }
