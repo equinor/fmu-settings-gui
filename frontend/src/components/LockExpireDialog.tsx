09f1d76b54bcf062128c22e739a7a7f12c52dd6c
nourinmohd <nourin_mohd97@outlook.com>
2026-02-24 13:18:20 +0100

    Api update

--- frontend/src/components/LockExpireDialog.tsx
+++ frontend/src/components/LockExpireDialog.tsx
@@ -1,29 +1,28 @@
 import { Button, Dialog } from "@equinor/eds-core-react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import {projectLockTimeoutWarningThreshold} from "#config";
+
+import type { LockStatus } from "#client";
+import {
+  projectGetLockStatusQueryKey,
+  projectPostLockRefreshMutation,
+} from "#client/@tanstack/react-query.gen";
+import { projectLockTimeoutWarningThreshold } from "#config";
+import { EditDialog, PageText } from "#styles/common";
 import { displayTimestamp } from "#utils/datetime";
 
-import { LockStatus } from "#client";
-import {
-  projectPostLockRefreshMutation, 
-  projectGetLockStatusQueryKey
-} from "#client/@tanstack/react-query.gen";
-import { EditDialog, PageText } from "#styles/common";
-
-export function LockExpireDialog({lockStatus}: {lockStatus: LockStatus})
-{
+export function LockExpireDialog({ lockStatus }: { lockStatus: LockStatus }) {
   const [isDialogOpen, setIsDialogOpen] = useState(false); // State to control dialog visibility
   const [timeUntilExpire, setTimeUntilExpire] = useState<number>(200000);
   const queryClient = useQueryClient();
-  const [isExpired, setIsExpired] = useState(false); 
+  const [isExpired, setIsExpired] = useState(false);
   // Extract lock information and expiration time
   const lockExpireAt = lockStatus.lock_info?.expires_at
     ? new Date(lockStatus.lock_info.expires_at * 1000).getTime()
     : undefined;
 
   // This is how define a mutation : refresh the lock
-    const { mutate, isPending } = useMutation({
+  const { mutate, isPending } = useMutation({
     ...projectPostLockRefreshMutation(),
     onSuccess: () => {
       void queryClient.invalidateQueries({
@@ -37,22 +36,24 @@ export function LockExpireDialog({lockStatus}: {lockStatus: LockStatus})
   });
 
   const onLockRefresh = () => {
-      mutate({});
+    mutate({});
   };
-  
-  // The Timer : TODO Add the icon to see how it looks 
+
+  // The Timer : TODO Add the icon to see how it looks
   useEffect(() => {
     if (lockExpireAt) {
       const updateTimeUntilExpire = () => {
         const timeLeft = Math.max(0, lockExpireAt - Date.now());
         setTimeUntilExpire(timeLeft);
 
-        timeLeft <= projectLockTimeoutWarningThreshold ? setIsDialogOpen(true) : setIsDialogOpen(false);
+        timeLeft <= projectLockTimeoutWarningThreshold
+          ? setIsDialogOpen(true)
+          : setIsDialogOpen(false);
       };
 
       updateTimeUntilExpire();
 
-      const interval = setInterval(updateTimeUntilExpire, 1000); // setInterval : countdown 
+      const interval = setInterval(updateTimeUntilExpire, 1000); // setInterval : countdown
 
       return () => {
         clearInterval(interval);
@@ -60,7 +61,7 @@ export function LockExpireDialog({lockStatus}: {lockStatus: LockStatus})
     }
   }, [lockExpireAt]);
   console.log("LockExpireDialog lockInfo:", lockStatus.lock_info);
-  console.log("LockExpireDialog lockExpireAt:", displayTimestamp(lockExpireAt) );
+  console.log("LockExpireDialog lockExpireAt:", displayTimestamp(lockExpireAt));
   console.log("LockExpireDialog rendered. Time until expire:", timeUntilExpire);
 
   useEffect(() => {
@@ -72,51 +73,51 @@ export function LockExpireDialog({lockStatus}: {lockStatus: LockStatus})
     }
   }, [timeUntilExpire, queryClient]);
 
-    return (
-      <EditDialog open={isDialogOpen} $minWidth="20em">
-        <Dialog.Header>
-          {isExpired ? "Lock expired" : "Lock about to expire"}
-        </Dialog.Header>
+  return (
+    <EditDialog open={isDialogOpen} $minWidth="20em">
+      <Dialog.Header>
+        {isExpired ? "Lock expired" : "Lock about to expire"}
+      </Dialog.Header>
 
-        <Dialog.Content>
-          {isExpired ? (
-            <PageText>
-              Your lock has expired.
-              Project is now read-only. It can be opened for editing from the project 
-              overview page. 
-            </PageText>
-          ) : (
-            <PageText>
-              Your lock will expire and the project will become read-only in{" "}. 
-              <b>{Math.ceil(timeUntilExpire / 1000)}</b> seconds. <br />
-              Do you want to extend the lock?
-            </PageText>
-          )}
-        </Dialog.Content>
+      <Dialog.Content>
+        {isExpired ? (
+          <PageText>
+            Your lock has expired. Project is now read-only. It can be opened
+            for editing from the project overview page.
+          </PageText>
+        ) : (
+          <PageText>
+            Your lock will expire and the project will become read-only in .
+            <b>{Math.ceil(timeUntilExpire / 1000)}</b> seconds. <br />
+            Do you want to extend the lock?
+          </PageText>
+        )}
+      </Dialog.Content>
 
-        <Dialog.Actions>
-          {isExpired ? (
-            <>
-          <Button onClick={onLockRefresh}>Extend Lock</Button>  
-          <Button
+      <Dialog.Actions>
+        {isExpired ? (
+          <>
+            <Button onClick={onLockRefresh}>Extend Lock</Button>
+            <Button
               onClick={() => {
                 setIsDialogOpen(false);
               }}
               variant="contained"
             >
               Release Lock
-            </Button>  </>) : 
+            </Button>{" "}
+          </>
+        ) : (
           <Button
-              onClick={() => {
-                setIsDialogOpen(false);
-              }}
-              variant="contained"
-            >
-              Ok
-            </Button> 
-          }
-            
-        </Dialog.Actions>
-          </EditDialog>
-        );
-    }
+            onClick={() => {
+              setIsDialogOpen(false);
+            }}
+            variant="contained"
+          >
+            Ok
+          </Button>
+        )}
+      </Dialog.Actions>
+    </EditDialog>
+  );
+}
