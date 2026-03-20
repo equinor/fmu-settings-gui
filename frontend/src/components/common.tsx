e8f89933f44459141b4048385137ffa345925cdd
Joar Grimstvedt <36162949+joargr@users.noreply.github.com>
2026-03-20 17:26:35 +0100

    ENH: Add generic error boundary component

--- frontend/src/components/common.tsx
+++ frontend/src/components/common.tsx
@@ -1,12 +1,88 @@
 import { Dialog } from "@equinor/eds-core-react";
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { useLocation } from "@tanstack/react-router";
+import { isAxiosError } from "axios";
+import type { ReactNode } from "react";
+import {
+  ErrorBoundary,
+  type FallbackProps,
+  getErrorMessage,
+} from "react-error-boundary";
 
 import { GeneralButton } from "#components/form/button";
-import { GenericDialog, PageText } from "#styles/common";
+import { GenericDialog, PageHeader, PageText } from "#styles/common";
+
+type ErrorFallbackProps = {
+  header?: string;
+  messages?: Record<number, string>;
+};
 
 export function Loading() {
   return <PageText>Loading...</PageText>;
 }
 
+function ErrorFallback({
+  error,
+  resetErrorBoundary,
+  header,
+  messages,
+}: FallbackProps & ErrorFallbackProps) {
+  const messageCodes = messages ? Object.keys(messages) : [];
+
+  return (
+    <>
+      {header !== undefined && <PageHeader>{header}</PageHeader>}
+
+      {messages !== undefined &&
+      isAxiosError(error) &&
+      error.response &&
+      messageCodes.includes(error.response.status.toString()) ? (
+        <PageText>{messages[error.response.status]}</PageText>
+      ) : (
+        <>
+          <PageText>An error occured: {getErrorMessage(error)}</PageText>
+
+          <PageText>Please try again.</PageText>
+
+          <GeneralButton label="Retry" onClick={resetErrorBoundary} />
+        </>
+      )}
+    </>
+  );
+}
+
+/**
+ * Defines an error boundary when using a query.
+ * @param children The children around which the error boundary is defined.
+ * @param header An optional header for the error message.
+ * @param messages An object for optionally defining error messages for individual HTTP status codes.
+ * @returns
+ */
+export function QueryErrorBoundary({
+  header,
+  messages,
+  children,
+}: ErrorFallbackProps & {
+  children: ReactNode;
+}) {
+  const location = useLocation();
+
+  return (
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          resetKeys={[location.pathname]}
+          onReset={reset}
+          fallbackRender={(props) => (
+            <ErrorFallback header={header} messages={messages} {...props} />
+          )}
+        >
+          {children}
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
+  );
+}
 export function ConfirmCloseDialog({
   isOpen,
   handleConfirmCloseDecision,
