b8dd0a6ad3cb04be30dd096842256c1cc04c6da1
nourinmohd <nourin_mohd97@outlook.com>
2026-07-20 09:16:48 +0200

    DEV: Update @hey-api/openapi-ts

--- frontend/openapi-ts.config.ts
+++ frontend/openapi-ts.config.ts
@@ -4,8 +4,39 @@ export default defineConfig({
   input: "http://localhost:8001/api/v1/openapi.json",
   output: {
     path: "src/client",
-    format: "biome",
-    lint: "biome",
+    postProcess: [
+      {
+        command: "tools/biome",
+        args: ["format", "--config-path=../biome.json", ".."],
+      },
+      {
+        command: "tools/biome",
+        args: ["lint", "--config-path=../biome.json", ".."],
+      },
+    ],
+    source: {
+      fileName: "openapi",
+      path: "./source",
+    },
+  },
+  parser: {
+    hooks: {
+      operations: {
+        isQuery: (op) => {
+          if (
+            op.method === "post" &&
+            [
+              "/api/v1/match",
+              "/api/v1/smda/field",
+              "/api/v1/smda/masterdata",
+              "/api/v1/smda/strat_units",
+            ].includes(op.path)
+          ) {
+            return true;
+          }
+        },
+      },
+    },
   },
   plugins: [
     ...defaultPlugins,
